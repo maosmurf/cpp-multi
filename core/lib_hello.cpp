fd3eb814250f16d99f559c74b9734065f12cf57e
Adam Zielinski <github@maosmurf.com>
2024-10-22 18:19:41 +0200

    lib_hello

---
+++ core/lib_hello.cpp
@@ -0,0 +1,7 @@
+#include "lib_hello.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ core/lib_hello.h
@@ -0,0 +1,6 @@
+#ifndef CORE_LIBRARY_H
+#define CORE_LIBRARY_H
+
+void hello();
+
+#endif //CORE_LIBRARY_H
